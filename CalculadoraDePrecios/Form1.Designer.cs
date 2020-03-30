ae79d62e22d6a8a3afecbf6d90873f3553c12e64
Daiana Stefania Ramirez <d.s.ramirez@outlook.com>
2020-03-30 10:42:26 -0300

    Initial commit

---
+++ CalculadoraDePrecios/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace CalculadoraDePrecios
+{
+    partial class btSalir
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(btSalir));
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblprecio = new System.Windows.Forms.Label();
+            this.lblseleccion = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.cbProductos = new System.Windows.Forms.ComboBox();
+            this.dgCompra = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PRODUCTO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PRECIOKG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PESO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MONTO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DESCUENTO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTap = new System.Windows.Forms.Label();
+            this.btPagar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.epError = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgCompra)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(250, 58);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(121, 21);
+            this.txtPrecio.TabIndex = 2;
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtPrecio_KeyPress);
+            this.txtPrecio.Validated += new System.EventHandler(this.TxtPrecio_Validated);
+            // 
+            // lblprecio
+            // 
+            this.lblprecio.AutoSize = true;
+            this.lblprecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprecio.Location = new System.Drawing.Point(15, 58);
+            this.lblprecio.Name = "lblprecio";
+            this.lblprecio.Size = new System.Drawing.Size(126, 15);
+            this.lblprecio.TabIndex = 4;
+            this.lblprecio.Text = "PRECIO POR KILO";
+            // 
+            // lblseleccion
+            // 
+            this.lblseleccion.AutoSize = true;
+            this.lblseleccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblseleccion.Location = new System.Drawing.Point(15, 26);
+            this.lblseleccion.Name = "lblseleccion";
+            this.lblseleccion.Size = new System.Drawing.Size(182, 15);
+            this.lblseleccion.TabIndex = 5;
+            this.lblseleccion.Text = "SELECCIONAR PRODUCTO";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(15, 94);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(108, 15);
+            this.lblPeso.TabIndex = 7;
+            this.lblPeso.Text = "PESO (Gramos)";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Location = new System.Drawing.Point(250, 94);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(121, 21);
+            this.txtPeso.TabIndex = 3;
+            this.txtPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtPeso_KeyPress);
+            this.txtPeso.Validated += new System.EventHandler(this.TxtPeso_Validated);
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAceptar.Image = ((System.Drawing.Image)(resources.GetObject("btAceptar.Image")));
+            this.btAceptar.Location = new System.Drawing.Point(451, 37);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(119, 62);
+            this.btAceptar.TabIndex = 9;
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.BtAceptar_Click);
+            // 
+            // cbProductos
+            // 
+            this.cbProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbProductos.FormattingEnabled = true;
+            this.cbProductos.Items.AddRange(new object[] {
+            "Lechuga",
+            "Tomate",
+            "Cebolla"});
+            this.cbProductos.Location = new System.Drawing.Point(250, 26);
+            this.cbProductos.Name = "cbProductos";
+            this.cbProductos.Size = new System.Drawing.Size(121, 23);
+            this.cbProductos.TabIndex = 1;
+            this.cbProductos.Validated += new System.EventHandler(this.CbProductos_Validated);
+            // 
+            // dgCompra
+            // 
+            this.dgCompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCompra.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.PRODUCTO,
+            this.PRECIOKG,
+            this.PESO,
+            this.MONTO,
+            this.DESCUENTO});
+            this.dgCompra.Location = new System.Drawing.Point(23, 157);
+            this.dgCompra.Name = "dgCompra";
+            this.dgCompra.Size = new System.Drawing.Size(686, 161);
+            this.dgCompra.TabIndex = 14;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // PRODUCTO
+            // 
+            this.PRODUCTO.HeaderText = "PRODUCTO";
+            this.PRODUCTO.Name = "PRODUCTO";
+            // 
+            // PRECIOKG
+            // 
+            this.PRECIOKG.HeaderText = "PRECIO POR KILO";
+            this.PRECIOKG.Name = "PRECIOKG";
+            // 
+            // PESO
+            // 
+            this.PESO.HeaderText = "PESO (GR)";
+            this.PESO.Name = "PESO";
+            // 
+            // MONTO
+            // 
+            this.MONTO.HeaderText = "MONTO";
+            this.MONTO.Name = "MONTO";
+            // 
+            // DESCUENTO
+            // 
+            this.DESCUENTO.HeaderText = "DESCUENTO";
+            this.DESCUENTO.Name = "DESCUENTO";
+            // 
+            // lblTap
+            // 
+            this.lblTap.AutoSize = true;
+            this.lblTap.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTap.Location = new System.Drawing.Point(527, 337);
+            this.lblTap.Name = "lblTap";
+            this.lblTap.Size = new System.Drawing.Size(146, 18);
+            this.lblTap.TabIndex = 15;
+            this.lblTap.Text = "TOTAL A PAGAR: ";
+            // 
+            // btPagar
+            // 
+            this.btPagar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btPagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPagar.Image = ((System.Drawing.Image)(resources.GetObject("btPagar.Image")));
+            this.btPagar.Location = new System.Drawing.Point(715, 157);
+            this.btPagar.Name = "btPagar";
+            this.btPagar.Size = new System.Drawing.Size(119, 62);
+            this.btPagar.TabIndex = 16;
+            this.btPagar.UseVisualStyleBackColor = true;
+            this.btPagar.Click += new System.EventHandler(this.BtPagar_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblseleccion);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.lblprecio);
+            this.groupBox1.Controls.Add(this.lblPeso);
+            this.groupBox1.Controls.Add(this.cbProductos);
+            this.groupBox1.Controls.Add(this.btAceptar);
+            this.groupBox1.Controls.Add(this.txtPeso);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(23, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(650, 128);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btCancelar.Image")));
+            this.btCancelar.Location = new System.Drawing.Point(715, 256);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(119, 62);
+            this.btCancelar.TabIndex = 18;
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.BtCancelar_Click);
+            // 
+            // epError
+            // 
+            this.epError.ContainerControl = this;
+            // 
+            // btSalir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(832, 450);
+            this.Controls.Add(this.btCancelar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btPagar);
+            this.Controls.Add(this.lblTap);
+            this.Controls.Add(this.dgCompra);
+            this.Name = "btSalir";
+            ((System.ComponentModel.ISupportInitialize)(this.dgCompra)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.epError)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblprecio;
+        private System.Windows.Forms.Label lblseleccion;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.ComboBox cbProductos;
+        private System.Windows.Forms.DataGridView dgCompra;
+        private System.Windows.Forms.Label lblTap;
+        private System.Windows.Forms.Button btPagar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PRODUCTO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PRECIOKG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PESO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MONTO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DESCUENTO;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.ErrorProvider epError;
+    }
+}
+
---
+++ CalculadoraDePrecios/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraDePrecios
+{
+    public partial class btSalir : Form
+    {
+        public btSalir()
+        {
+            InitializeComponent();
+        }
+        private void TxtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Unicamente se aceptan números");
+            }
+        }
+
+        private void TxtPeso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsNumber(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Unicamente se aceptan números");
+            }
+        }
+
+        private void TxtPrecio_Validated(object sender, EventArgs e)
+        {
+            if (txtPrecio.Text.Trim() == "")
+            {
+                epError.SetError(txtPrecio, "Introducir precio");
+                txtPrecio.Focus();
+            }
+        }
+
+        private void TxtPeso_Validated(object sender, EventArgs e)
+        {
+            if (txtPeso.Text.Trim() == "")
+            {
+                epError.SetError(txtPeso, "Introducir peso");
+                txtPeso.Focus();
+            }
+        }
+
+        private void CbProductos_Validated(object sender, EventArgs e)
+        {
+            if (cbProductos.Items.Count <= 0)
+            {
+                epError.SetError(cbProductos, "Seleccionar producto");
+                cbProductos.Focus();
+            }
+        }
+
+        private void BtAceptar_Click(object sender, EventArgs e)
+        {
+            Decimal Peso = Convert.ToDecimal(txtPeso.Text);
+            Decimal Precio = Convert.ToDecimal(txtPrecio.Text);
+            Decimal Monto = Peso * Precio / 1000;
+            Decimal Descuento = Monto * 10 / 100;
+
+            if (Monto >= 100)
+            {
+                Descuento = Monto * 10 / 100;
+            }
+            else
+            {
+                Descuento = 0;
+            }
+
+            dgCompra.Rows.Add(cbProductos.SelectedIndex +1, cbProductos.SelectedItem, txtPrecio.Text, txtPeso.Text, 
+                Convert.ToString(Monto), Convert.ToString(Descuento));
+
+            txtPeso.Clear();
+            txtPrecio.Clear();
+            cbProductos.Focus();
+        }
+
+        private void BtPagar_Click_1(object sender, EventArgs e)
+        {
+            Decimal MontoTotal = 0;
+            Decimal DescuentoTotal = 0;
+
+            foreach (DataGridViewRow Row in dgCompra.Rows)
+            {
+                MontoTotal = MontoTotal + Convert.ToDecimal(Row.Cells["MONTO"].Value);
+                DescuentoTotal = DescuentoTotal + Convert.ToDecimal(Row.Cells["DESCUENTO"].Value);
+            }
+
+            lblTap.Text = "TOTAL A PAGAR: $" + (MontoTotal - DescuentoTotal);
+        }
+
+        private void BtCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
